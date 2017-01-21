b30a82dcdcf20f74b47fc5fd86e24adceffe6f73
byeskille <oyvind@byeskille.net>
2017-01-21 14:00:14 +0100

    add backup of cert before renew
    backing up cert in production before doing renew process overwriting
    signed.crt file

--- smartRenew.sh
+++ smartRenew.sh
@@ -3,7 +3,7 @@
 # The remaining life on our certificate below which we should renew (7 days).
 RENEW=7
 # Set date for backup
-TODAY=`date '+%Y_%m_%d'`;
+TODAY=`date '+%Y_%m_%d'`
 # If the certificate has less life remaining than we want.
 if ! openssl x509 -checkend $[ 86400 * $RENEW ] -noout -in /path/to/signed.crt
         then
